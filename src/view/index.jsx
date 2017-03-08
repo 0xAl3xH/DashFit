05e81e8c2ac29d45f0bb02f6759c0692c756b124
Alex Huang <alexh95@mit.edu>
2017-03-09 01:35:39 +0800

    Change import to account for rename

--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import HelloWorld from './components/Test/hello-world.jsx'
+import HelloWorld from './components/Test/HelloWorld.jsx'
 
 render(<HelloWorld/>, document.getElementById('app'));
