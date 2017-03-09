1d001fe2ba43d64af177da432668183a1c486c28
Alex Huang <alexh95@mit.edu>
2017-03-10 04:15:47 +0800

    Make NavItem more react router friendly and styled

--- client/src/view/components/Nav/NavItem/NavItem.jsx
+++ client/src/view/components/Nav/NavItem/NavItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './NavItem.less';
+import {Link} from 'react-router';
 
 export default class NavItem extends React.Component {
   render () {
     return ( 
       <div className = "nav-item">
-        { this.props.name }
+        <Link to = {this.props.to} className = "nav-item-link">{this.props.name}</Link>
       </div>
     );
   }
