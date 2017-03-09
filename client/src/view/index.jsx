6bbc15b74c70bef9ddd254c8c6594afd16361c90
Alex Huang <alexh95@mit.edu>
2017-03-09 18:01:26 +0800

    Incorporate Skeleton boilerplate for better default styling

--- client/src/view/index.jsx
+++ client/src/view/index.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import HelloWorld from './components/Test/HelloWorld.jsx';
-  
+
+//Use skeleton boilerplate
+import './shared_styles/skeleton.less'
+
 render(<HelloWorld/>, document.getElementById('app'));
