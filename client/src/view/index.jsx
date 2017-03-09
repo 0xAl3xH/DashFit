5b1c464b13ea7b4ec8084b390a71c259b1f8792c
Alex Huang <alexh95@mit.edu>
2017-03-10 02:37:53 +0800

    Test react router with / and /page

--- client/src/view/index.jsx
+++ client/src/view/index.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Router, Route, Link, hashHistory} from 'react-router';
 
+// Import React components
 import HelloWorld from './components/Test/HelloWorld.jsx';
+import Header from './components/Header/Header.jsx';
 import Nav from './components/Nav/Nav.jsx';
+import NavItem from './components/Nav/NavItem/NavItem.jsx';
+import MainContent from './components/MainContent/MainContent.jsx';
 
-//Use skeleton boilerplate
+// Use Skeleton styling boilerplate
 import './shared_styles/skeleton.less'
+
+const Home = () => (
+  <h2>Home page</h2>
+);
+
+const Page = () => (
+  <h2>Some other page</h2>
+);
 
 class App extends React.Component {
   render () {
     return (
-      <Nav>
-        <HelloWorld/>
-        <HelloWorld/>
-      </Nav>
+      <div>
+        <Header> 
+          DashFit 
+        </Header>
+        <Nav>
+          <NavItem name = "Log Weight"/>
+          <NavItem name = "Visualize"/>
+        </Nav>
+        <MainContent id = "main">
+          <Router history={hashHistory}>
+            <Route path = "/" component = {Home}/>
+            <Route path = "/page" component = {Page}/>
+          </Router>
+        </MainContent>
+      </div>
     );
   }
 }
