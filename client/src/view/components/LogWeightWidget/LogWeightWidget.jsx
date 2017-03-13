f47ffe0436bbba124b436d7bc95bf789bbdd76a4
Alex Huang <alexh95@mit.edu>
2017-03-13 23:39:44 +0800

    Get UI talking to backend and rendering responses correctly

--- client/src/view/components/LogWeightWidget/LogWeightWidget.jsx
+++ client/src/view/components/LogWeightWidget/LogWeightWidget.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './LogWeightWidget.less';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.less';
 
 import MainContent from '../MainContent/MainContent.jsx';
 import Title from '../MainContent/Title/Title.jsx';
@@ -8,60 +11,87 @@ export default class LogWeight extends React.Component {
   constructor() {
     super();
     this.state = {
-      average: null,
+      average: undefined,
       weights: {
-      }
+      },
+      curDate: moment(),
+      rows:[]
     };
+  }
+  
+  handleChange(date) {
+    this.setState({
+      curDate: date
+    });
   }
   
   componentDidMount() {
     //Get weight data
-    this.setState({
-      weights: {
-        Monday:176.0,
-        Tuesday:175.4,
-        Wednesday:175.4,
-        Thursday:174.9        
-      }
+    this.getWeek().then(res =>{
+      return res.json();
+    }).then(records => {
+      this.setState({average: this.calcAverage(records)});
+      var rows = this.renderRows(records);
+      this.setState({rows:rows});
     });
   }
   
-  calcAverage(weights) {
-    const weightsSum = Object.values(weights).reduce((a,b) => a + b, 0);
-    var average = weightsSum/Object.keys(weights).length;
+  calcAverage(records) {
+    const weightsSum = records.reduce((a, b) => ({weight: a.weight + b.weight})).weight;
+    var average = weightsSum/records.length;
     average = (Math.round(average * 10) / 10).toFixed(1);
-    if (weights) 
-      return average
-    return null
+    return average
+  }
+  
+  getWeek(date) {
+    var myHeaders = {
+      "Content-Type":'application/json'
+    };
+    
+    return fetch('/weight',{
+      method: 'GET',
+      headers: myHeaders,
+    });
+  }
+  
+  renderRows(records) {
+  var rows = [];
+  for (var i = 0; i < records.length; i++) {
+    rows.push(
+      <tr key={i}>
+        <td>{moment(records[i].time).format('M/D')}</td>
+        <td>{records[i].weight.toFixed(1)}</td>
+      </tr>);
+  }  
+  return rows;
   }
   
   render () {
     const weights = this.state.weights;
-    const average = this.state.average ? this.state.average: this.calcAverage(weights);
-    var rows = [];
-    for (var i = 0; i < Object.keys(weights).length; i++) {
-      var day = Object.keys(weights)[i];
-      rows.push(
-        <tr key={i}>
-          <td>{day}</td>
-          <td>{weights[day].toFixed(1)}</td>
-        </tr>);
-    }
     return ( 
       <MainContent>
         <Title>Weight Log</Title>
-        <div>Current Weekly Average: { average }</div>
-        <table className = "u-full-width">
+        <div>
+          Select a date: <DatePicker
+        selected={this.state.curDate}
+        onChange={this.handleChange.bind(this)}/>
+        </div>
+        <div>Weekly Average: { this.state.average }</div>
+        <table  className = "u-full-width weight-table">
           <thead>
             <tr>
-              <th>Day</th>
+              <th>Date</th>
               <th>Weight</th>
             </tr>
           </thead>
           <tbody>
-            { rows }
+            {this.state.rows}
           </tbody>
         </table>
+        <div className = "three columns">
+          Today's weight: <input className="u-full-width" type="text"/>
+          <input className="button-primary" type="submit" value="Submit"/>
+        </div>
       </MainContent>
     );
   }
