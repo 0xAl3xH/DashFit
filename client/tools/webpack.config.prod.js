135cc01d1e6e61a12e4ff86d211c83fe263ff785
Alex H <alexh95@mit.edu>
2017-06-22 02:23:33 -0400

    Add production webpack configurations

---
+++ client/tools/webpack.config.prod.js
@@ -0,0 +1,72 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const webpack = require('webpack');
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, '../src/view');
+const BUILD_DIR = path.resolve(__dirname, '../build');
+
+//Plugin to turn LESS into static styles.css, stored in output path specified in config
+const extractLESS = new ExtractTextPlugin('styles.css');
+
+var config = {
+  resolve: {
+    // Since our build tools and modules are not in the root dir relative to entry,
+    // add the directory to be resolved so WebPack finds it
+    modules: [
+      path.resolve(__dirname,'node_modules')
+    ],
+    extensions: ['.js', '.jsx'],
+    alias: {
+      components: path.resolve(__dirname, '../src/view/components'),
+      shared_styles: path.resolve(__dirname,'../src/view/shared_styles')  //Prepend import with ~ for less-loader e.g ~shared_styles
+    }
+  },
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      //JSX to JS
+      {
+        test: /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader',
+        query: {
+          // Presets have to be resolved since our node_modules is not at root
+          presets: [
+            'babel-preset-es2015',
+            'babel-preset-react',
+          ].map(require.resolve),
+        }
+      },
+      //LESS to static CSS file
+      {
+          test: /\.less$/,
+          use: extractLESS.extract(['css-loader','less-loader']),
+      },
+      {
+        test: /\.jsx?/,
+        loader : './styling-loader.js',
+        query: {
+          ignoreNodeModules: true
+        }
+      }
+    ]
+  },
+  plugins: [
+    extractLESS,
+    new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(), //dedupe similar code 
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin()//Merge chunks  
+  ]
+}
+
+module.exports = config;
